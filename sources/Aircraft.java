1b130b10f72ea35de6a1d6e265a47e35bd0f46ac
Manuel Jiménez Rodríguez <majiro1996@gmail.com>
2025-02-03 15:10:05 +0100

    added aircraft class that extends the flyable class

---
+++ sources/Aircraft.java
@@ -0,0 +1,23 @@
+package avaj.sources;
+
+
+
+public class Aircraft extends Flyable
+{
+    protected long id;
+    protected String name;
+    protected Coordinates coordinates;
+
+    protected Aircraft(long id, String name, Coordinates coordinates)
+    {
+        this.id = id;
+        this.name = name;
+        this.coordinates = coordinates;
+    }
+
+    @Override
+    public void updateConditions()
+    {
+        // todo
+    }
+}
