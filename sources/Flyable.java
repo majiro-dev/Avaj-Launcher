6414bb4f95f407b6e907b0a59bd82ed118688b6b
Manuel Jiménez Rodríguez <majiro1996@gmail.com>
2025-02-03 14:55:19 +0100

    added flyable as an abstract class

---
+++ sources/Flyable.java
@@ -0,0 +1,13 @@
+
+package avaj.sources;
+
+public abstract class Flyable
+{
+    protected WeatherTower weatherTower;
+
+    abstract public void updateConditions();
+    public void registerTower(WeatherTower weatherTower)
+    {
+        this.weatherTower = weatherTower;
+    }
+}
