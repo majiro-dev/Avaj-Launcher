d2aa33524332aba953275ea25f9249198f57ff6d
majiro-dev <majiro.dev@gmail.com>
2025-03-06 13:41:13 +0100

    Validation of the scenario file before starting the simulations

--- sources/Launcher.java
+++ sources/Launcher.java
@@ -1,6 +1,8 @@
 package avaj.sources;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Launcher 
 {
@@ -53,12 +55,35 @@ public class Launcher
     {
         String line;
         AircraftFactory factory = AircraftFactory.getInstance();
+        List <Flyable> flyables = new ArrayList<Flyable>();
         weatherTower = new WeatherTower();
+        int lineCount = 0;
         while ((line = reader.readLine()) != null) 
         {
+            if (lineCount == 0 && !line.matches("\\d+")) 
+            {
+                System.out.println("First line must be a positive integer");
+                return;
+            }
+            else if (lineCount == 0) 
+            {
+                simulations = Integer.parseInt(line);
+                lineCount++;
+                continue;
+            }
             String[] parts = line.split(" ");
             if (parts.length == 5) 
             {
+                if (!parts[0].matches("Baloon|JetPlane|Helicopter")) 
+                {
+                    System.out.println("Invalid aircraft type: " + parts[0]);
+                    return;
+                }
+                if (!parts[2].matches("\\d+") || !parts[3].matches("\\d+") || !parts[4].matches("\\d+")) 
+                {
+                    System.out.println("Invalid coordinates: " + parts[2] + ", " + parts[3] + ", " + parts[4]);
+                    return;
+                }
                 String type = parts[0];
                 String name = parts[1];
                 int longitude = Integer.parseInt(parts[2]);
@@ -66,12 +91,18 @@ public class Launcher
                 int height = Integer.parseInt(parts[4]);
                 Coordinates coordinates = new Coordinates(longitude, latitude, height);
                 Flyable aircraft = factory.newAircraft(type, name, coordinates);
-                aircraft.registerTower(weatherTower);
+                flyables.add(aircraft);
             }
-            else if (parts.length == 1 && parts[0].matches("\\d+")) 
+            else 
             {
-                simulations = Integer.parseInt(parts[0]);
+                System.out.println("Invalid line: " + line);
+                return;
             }
+        }
+
+        for (Flyable aircraft : flyables) 
+        {
+            aircraft.registerTower(weatherTower);
         }
     }
 
